ee35053ad472b8c89e96314c932504b0e833f66f
sabina1010 <sabina.z.ahmadova@gmail.com>
2022-09-10 19:03:39 +0400

    DONE

---
+++ src/main/java/company/Main.java
@@ -0,0 +1,28 @@
+package company;
+
+import company.dao.FamilyService;
+import company.dao.impl.CollectionFamilyDao;
+import company.humans.Man;
+import company.humans.Woman;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Human mother = new Woman();
+        Human father= new Man("Father", "f", "20/03/2009",12);
+        Family family=new Family(mother,father);
+        Human child= new Human();
+        FamilyService familyService = new FamilyService(new CollectionFamilyDao());
+        familyService.createNewFamily(mother, father);
+        System.out.println(familyService.count());
+        System.out.println(familyService.getAllFamilies());
+        System.out.println(familyService.countFamiliesWithMemberNumber(2));
+        System.out.println(familyService.getFamilyById(0));
+
+
+    }
+
+}
---
+++ src/main/java/company/humans/Woman.java
@@ -0,0 +1,31 @@
+package company.humans;
+
+import company.DayOfWeeks;
+import company.Family;
+import company.Human;
+
+import java.util.Map;
+
+public final class Woman extends Human {
+    public void makeUp(){
+        System.out.println("Sabina loves makeUp!");
+
+    }
+    public void greetPet(){
+        System.out.println("Hello from Ms." + super.getName());
+    }
+    public Woman (){
+
+    }
+    public Woman(String name, String surname, String dateOfBirth, int iq){
+        super(name,surname, dateOfBirth,iq);
+    }
+    public Woman(String name, String surname, int year, int iq, Map<DayOfWeeks, String> schedule, Family family){
+        super(name,surname,year,iq,schedule,family);
+    }
+    public Woman(String name, String surname, int year){
+        super(name,surname,year);
+    }
+
+
+}
